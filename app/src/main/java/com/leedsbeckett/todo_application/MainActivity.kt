bce72b9f344976445e068450c2a66cd4a3d71641
Aryan Prince <aryanprincesanty@icloud.com>
2023-05-12 01:41:38 +0100

    feat: Implemented adding tasks to `ListView`

--- app/src/main/java/com/leedsbeckett/todo_application/MainActivity.kt
+++ app/src/main/java/com/leedsbeckett/todo_application/MainActivity.kt
@@ -2,10 +2,39 @@ package com.leedsbeckett.todo_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextTask: EditText
+    private lateinit var buttonAddTask: Button
+    private lateinit var taskListView: ListView
+
+    private var taskList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextTask = findViewById(R.id.editTextTask)
+        buttonAddTask = findViewById(R.id.buttonAddTask)
+        taskListView = findViewById(R.id.listViewTasks)
+
+        buttonAddTask.setOnClickListener {
+            val task = editTextTask.text.toString()
+            if (task.isNotEmpty()) {
+                taskList.add(task)
+                editTextTask.text.clear()
+                updateTaskList()
+            }
+        }
+    }
+
+    private fun updateTaskList() {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, taskList)
+        taskListView.adapter = adapter
     }
 }
