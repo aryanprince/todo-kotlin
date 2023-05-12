4f3ed14437ecbcd8adc85e3a2dbeee099b120309
Aryan Prince <aryanprincesanty@icloud.com>
2023-05-12 02:33:19 +0100

    feat(database): Added `Task` entity for Room DB

---
+++ app/src/main/java/com/leedsbeckett/todo_application/Task.kt
@@ -0,0 +1,10 @@
+package com.leedsbeckett.todo_application
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String
+)
